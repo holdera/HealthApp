07710e852920c3091f086283460c11463be6a359
holdera <holdera2@hotmail.com>
2014-05-08 05:54:26 -0400

    added pages

---
+++ chatroom.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="keywords" content="health, fitness, exercise, lifestyle, healthy, weightloss">
+	<meta name="description" content="spreading awareness of living a healthy lifestyle,
+	 lose weight, build muscle, its not a diet, its a lifestyle change">
+	<meta name="viewport" content="width=device-width, user-scalable=no">
+	<title>The Healthy LifeStyle</title>
+
+
+	<?php //include_once 'template/header.php'; ?>
+</head>
+<body>
+
+
+<section class="row">
+
+
+<section class="col-md-6 welcome">
+<h1>Change Your Lifestyle!</h1>
+
+<p>Fingerstache wayfarers mlkshk, flexitarian skateboard paleo banh mi Austin. 
+Fingerstache synth gastropub art party cornhole sriracha, iPhone Odd Future. Blue Bottle church-key roof party 
+fingerstache tofu banh mi. Jean shorts ethical craft beer, Thundercats hella ennui tote bag High Life Wes Anderson. 
+Ethical seitan cliche, chillwave artisan Odd Future iPhone lo-fi art party fingerstache flexitarian locavore meh kale chips. 
+Williamsburg Tumblr kitsch bicycle rights, Shoreditch craft beer distillery. Banh mi YOLO quinoa, Banksy Neutra drinking vinegar 
+dreamcatcher stumptown Etsy blog organic McSweeney's banjo.</p>
+</section><!--end of welcome-->
+
+<section class="col-md-6 homeIMG">
+<h1>Change Your Lifestyle!</h1>
+
+<p>Fingerstache wayfarers mlkshk, flexitarian skateboard paleo banh mi Austin. 
+Fingerstache synth gastropub art party cornhole sriracha, iPhone Odd Future. Blue Bottle church-key roof party 
+fingerstache tofu banh mi. Jean shorts ethical craft beer, Thundercats hella ennui tote bag High Life Wes Anderson. 
+Ethical seitan cliche, chillwave artisan Odd Future iPhone lo-fi art party fingerstache flexitarian locavore meh kale chips. 
+Williamsburg Tumblr kitsch bicycle rights, Shoreditch craft beer distillery. Banh mi YOLO quinoa, Banksy Neutra drinking vinegar 
+dreamcatcher stumptown Etsy blog organic McSweeney's banjo.</p>
+</section><!--end of homeIMG-->
+
+</section><!--end of row-->
+
+<?php // include_once 'template/footer.php'; ?>
+</body>
+</html>
--- template/homefooter.php
+++ template/homefooter.php
@@ -1,9 +1,5 @@
 <footer class="row">
 
-<section class="row">
-<h3>Connect With us</h3>
-</section>
-
 <form method="post" action="subscribe.php" class="forms">
 	<label for="name">Name: </label>
 	<input type="text" name="name"/>
