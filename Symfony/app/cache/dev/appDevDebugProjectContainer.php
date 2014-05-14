282526dcb12e0000efff406ec56eb66b8b8b3ae8
holdera <holdera2@hotmail.com>
2014-05-13 21:33:57 -0400

    fixed issues, but can't create connection

--- Symfony/app/cache/dev/appDevDebugProjectContainer.php
+++ Symfony/app/cache/dev/appDevDebugProjectContainer.php
@@ -3833,7 +3833,7 @@ class appDevDebugProjectContainer extends Container
 
             ),
             'assetic.java.bin' => '/usr/bin/java',
-            'assetic.node.bin' => '/usr/bin/node',
+            'assetic.node.bin' => '/usr/local/bin/node',
             'assetic.ruby.bin' => '/usr/bin/ruby',
             'assetic.sass.bin' => '/usr/bin/sass',
             'assetic.filter.cssrewrite.class' => 'Assetic\\Filter\\CssRewriteFilter',
