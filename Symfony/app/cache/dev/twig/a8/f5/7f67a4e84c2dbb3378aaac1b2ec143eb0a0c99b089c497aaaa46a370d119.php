e5f5db3e3175ff65ef685ea740d7f727c12c467f
holdera <holdera2@hotmail.com>
2014-05-14 12:02:32 -0400

    yeh

--- Symfony/app/cache/dev/twig/a8/f5/7f67a4e84c2dbb3378aaac1b2ec143eb0a0c99b089c497aaaa46a370d119.php
+++ Symfony/app/cache/dev/twig/a8/f5/7f67a4e84c2dbb3378aaac1b2ec143eb0a0c99b089c497aaaa46a370d119.php
@@ -21,7 +21,21 @@ class __TwigTemplate_a8f57f67a4e84c2dbb3378aaac1b2ec143eb0a0c99b089c497aaaa46a37
     fingerstache tofu banh mi. Jean shorts ethical craft beer, Thundercats hella ennui tote bag High Life Wes Anderson.
     Ethical seitan cliche, chillwave artisan Odd Future iPhone lo-fi art party fingerstache flexitarian locavore meh kale chips.
     Williamsburg Tumblr kitsch bicycle rights, Shoreditch craft beer distillery. Banh mi YOLO quinoa, Banksy Neutra drinking vinegar
-    dreamcatcher stumptown Etsy blog organic McSweeney's banjo.</p>";
+    dreamcatcher stumptown Etsy blog organic McSweeney's banjo.</p>
+
+<p>   database_driver: pdo_mysql
+    database_host: 50.62.209.8
+    database_port: '3306'
+    database_name: HealthApp
+    database_user: APSquared
+    database_password: K5-dynabagyxu
+    mailer_transport: smtp
+    mailer_host: 127.0.0.1
+    mailer_user: null
+    mailer_password: null
+    locale: en
+    secret: 6232749637906b8a5361b6de29d800698b23c4af
+    database_path: null</p>";
     }
 
     public function getTemplateName()
