0ed0078649865da95bd7a4e3d0cfdbd3e6a0f9a7
holdera <holdera2@hotmail.com>
2014-06-16 19:46:14 -0400

    Revert 92c09c2..7d43b16
    This rolls back to commit 92c09c26e7003510ae5adf330bf9b95429ead5f6.

--- Symfony/app/cache/dev/assetic/config/6/668d53ca1443fb06f4c7cdc5aba0e7ba.php
+++ Symfony/app/cache/dev/assetic/config/6/668d53ca1443fb06f4c7cdc5aba0e7ba.php
@@ -2,25 +2,4 @@
 
 // healthLifestyleBundle:Lifestyle:lifestyle.html.twig
 return array (
-  '8b11947' => 
-  array (
-    0 => 
-    array (
-      0 => '@healthLifestyleBundle/Resources/public/css/*',
-    ),
-    1 => 
-    array (
-      0 => 'cssrewrite',
-    ),
-    2 => 
-    array (
-      'output' => '_controller/css/8b11947.css',
-      'name' => '8b11947',
-      'debug' => NULL,
-      'combine' => NULL,
-      'vars' => 
-      array (
-      ),
-    ),
-  ),
 );
